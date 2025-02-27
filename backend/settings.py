8b738ab5c0149a053de8006f18824afb63fd7641
Rodrigo Santos <rodrigo.scosta@sinqia.com.br>
2025-02-27 20:06:53 -0300

    desfazendo as alterações de banco

--- backend/settings.py
+++ backend/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
